91135e7ff5437095b460bcc6a4eb2ef92d0d6995
enzotiezzi <enzo_tiezzi@hotmail.com>
2015-11-02 00:49:15 -0200

    adicionando projeto

---
+++ gimeses/Controllers/ExecucaoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gimeses.Controllers
+{
+    public class ExecucaoController : Controller
+    {
+        // GET: Execucao
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Qualidade()
+        {
+
+            return View();
+        }
+
+        public ActionResult Aquisicao()
+        {
+            return View();
+        }
+    }
+}
---
+++ gimeses/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("gimeses")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gimeses")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cc0ad656-1bc2-4d14-aa10-cef4ae919848")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
